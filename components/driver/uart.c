a41f410bfaa6563bbfbf9cfd5207d5cc59e6cde7
Stuart Baker <stuart_w_baker@ymail.com>
2024-01-14 13:23:02 -0600

    Disable UART receiver whenever TX is active in mode UART_MODE_RS485_HALF_DUPLEX.

--- components/driver/uart.c
+++ components/driver/uart.c
@@ -808,6 +808,7 @@ static uint32_t UART_ISR_ATTR uart_enable_tx_write_fifo(uart_port_t uart_num, co
     uint32_t sent_len = 0;
     UART_ENTER_CRITICAL_SAFE(&(uart_context[uart_num].spinlock));
     if (UART_IS_MODE_SET(uart_num, UART_MODE_RS485_HALF_DUPLEX)) {
+        uart_context[uart_num].hal.dev->clk_conf.rx_sclk_en = 0;
         uart_hal_set_rts(&(uart_context[uart_num].hal), 0);
         uart_hal_ena_intr_mask(&(uart_context[uart_num].hal), UART_INTR_TX_DONE);
     }
@@ -1097,6 +1098,7 @@ static void UART_ISR_ATTR uart_rx_intr_handler_default(void *param)
                 if (UART_IS_MODE_SET(uart_num, UART_MODE_RS485_HALF_DUPLEX)) {
                     uart_hal_rxfifo_rst(&(uart_context[uart_num].hal));
                     uart_hal_set_rts(&(uart_context[uart_num].hal), 1);
+                    uart_context[uart_num].hal.dev->clk_conf.rx_sclk_en = 1;
                 }
                 UART_EXIT_CRITICAL_ISR(&(uart_context[uart_num].spinlock));
                 xSemaphoreGiveFromISR(p_uart_obj[uart_num]->tx_done_sem, &HPTaskAwoken);
