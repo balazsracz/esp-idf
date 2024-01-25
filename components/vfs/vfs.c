bd85ac1cf47b6d7a9ea60646aa6fc674f8aa2d10
Stuart Baker <stuart_w_baker@ymail.com>
2024-01-24 20:25:45 -0600

    Improve efficiency by replacing a divide operation with an addition operation.

--- components/vfs/vfs.c
+++ components/vfs/vfs.c
@@ -1042,12 +1042,10 @@ int esp_vfs_select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *errorfds
             if (timeout->tv_sec == 0 && timeout->tv_usec == 0) {
                 ticks_to_wait = 0;
             } else {
+                /* Round up sub-msec resolution */
                 uint32_t timeout_ms =
-                    (timeout->tv_sec * 1000) + (timeout->tv_usec / 1000);
-                if (timeout->tv_usec % 1000) {
-                    /* Round up sub-msec resolution. */
-                    ++timeout_ms;
-                }
+                    (timeout->tv_sec * 1000) +
+                    ((timeout->tv_usec + 999) / 1000);
                 /* Round up the number of ticks.
                  * Not only we need to round up the number of ticks, but we also
                  * need to add 1. Indeed, `select` function shall wait for AT
